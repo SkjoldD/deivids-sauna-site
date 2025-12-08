e920200a2b3a1da52040bf52d6917a7e63454575
SkjoldD <sk.dyre@gmail.com>
2025-12-08 23:32:50 +0100

    github pages added

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sauna-site/', // GitHub Pages repository name
   server: {
     port: 3000,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
   css: {
@@ -20,4 +21,4 @@ export default defineConfig({
       }
     }
   }
-});
+})
